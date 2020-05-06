134816f70ac420155d212f2c711118193e7fa1ca
Carlos Hurtado <gabo_duty@hotmail.com>
2020-05-06 17:57:03 -0500

    formaulario de registro

--- app/Http/Controllers/EstudianteController.php
+++ app/Http/Controllers/EstudianteController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Estudiante;
+use App\Models\Programa;
 
 class EstudianteController extends Controller
 {
-    //
+    public function formularioRegistro(){
+
+        $programas = Programa::all();
+
+        return view('', compact($programas));
+    }
 }
